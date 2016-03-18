f643bde049575ad74f9a94b905f9281dea0fa3d8
Facundo Galan <facundogalan91@gmail.com>
2016-03-17 23:53:34 -0300

    Practice.

---
+++ icpcarchive.ecs.baylor.edu/Assignments.cpp
@@ -0,0 +1,49 @@
+/*
+        By: facug91
+        From: https://icpcarchive.ecs.baylor.edu/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=4523
+        Name: Assignments
+        Date: 17/03/2016
+*/
+
+#include <bits/stdc++.h>
+#define endl "\n"
+#define EPS 1e-9
+#define MP make_pair
+#define F first
+#define S second
+#define DB(x) cerr << " #" << (#x) << ": " << (x)
+#define DBL(x) cerr << " #" << (#x) << ": " << (x) << endl
+const double PI = acos(-1.0);
+
+#define INF 1000000000
+//#define MOD 1000000007ll
+//#define MAXN 1000005
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long llu;
+typedef pair<int, int> ii; typedef pair<ii, ii> iiii;
+typedef vector<int> vi; typedef vector<ii> vii; typedef vector<iiii> viiii;
+
+ll n, d, v, f, c, ans;
+
+int main () {
+	#ifdef ONLINE_JUDGE
+		ios_base::sync_with_stdio(0); cin.tie(0);
+	#endif
+	//cout<<fixed<<setprecision(3); //cerr<<fixed<<setprecision(3); //cin.ignore(INT_MAX, ' '); //cout << setfill('0') << setw(5)
+	int tc = 1, i, j;
+	
+	cin>>tc;
+	while (tc--) {
+		cin>>n>>d;
+		ans = 0;
+		while (n--) {
+			cin>>v>>f>>c;
+			if (d * c <= f * v) ans++;
+		}
+		cout<<ans<<endl;
+	}
+	
+	return 0;
+}
